52221b6d500d98393b87ba0da48e625e529621dc
@Laiiif <danimatto610@gmail.com>
2024-02-05 23:40:57 +0100

    Aggiunta PuntoInteresse.java, Ruolo.java, Utente.java, Main.java

---
+++ src/main/java/org/example/PuntoInteresse.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class PuntoInteresse {
+
+    private String nome;
+    private String descrizione;
+    private String indirizzo;
+    private String orarioApertura;
+    private String orarioChiusura;
+    private float valutazioneMedia;
+
+    public PuntoInteresse(String nome, String descrizione, String indirizzo, String orarioApertura, String orarioChiusura, float valutazioneMedia) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.indirizzo = indirizzo;
+        this.orarioApertura = orarioApertura;
+        this.orarioChiusura = orarioChiusura;
+        this.valutazioneMedia = valutazioneMedia;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public String getDescrizione () {
+        return descrizione;
+    }
+
+    public String getIndirizzo () {
+        return indirizzo;
+    }
+
+    public String getOrarioApertura () {
+        return orarioApertura;
+    }
+
+    public String getOrarioChiusura () {
+        return orarioChiusura;
+    }
+
+    public float getValutazioneMedia () {
+        return valutazioneMedia;
+    }
+
+}
---
+++ src/main/java/org/example/Ruolo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public interface Ruolo {
+    public enum RuoloEnum {
+        Contributor, ContributorAutorizzato, TuristaAutenticato, TuristaNonAutenticato, Animatore, Curatore,
+        GestoreDellaPiattaforma, ResponsabileTerritorioComunale;
+    }
+
+
+
+
+}
