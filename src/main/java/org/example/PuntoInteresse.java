1fd94f601040395f0b6431d3f219902f2486f825
@Laiiif <danimatto610@gmail.com>
2024-02-05 23:51:51 +0100

    Aggiunta PuntoInteresse.java, Ruolo.java, Utente.java, Main.java

--- src/main/java/org/example/PuntoInteresse.java
+++ src/main/java/org/example/PuntoInteresse.java
@@ -1,45 +1,77 @@
-package org.example;
-
 public class PuntoInteresse {
-
     private String nome;
     private String descrizione;
+    private String categoria;
     private String indirizzo;
-    private String orarioApertura;
-    private String orarioChiusura;
-    private float valutazioneMedia;
+    private String sitoWeb;
+    private String telefono;
+    private String email;
+    private String orari;
+    private String costo;
+    private String foto;
+    private String latitudine;
+    private String longitudine;
 
-    public PuntoInteresse(String nome, String descrizione, String indirizzo, String orarioApertura, String orarioChiusura, float valutazioneMedia) {
+    public PuntoInteresse(String nome, String descrizione, String categoria, String indirizzo, String sitoWeb, String telefono, String email, String orari, String costo, String foto, String latitudine, String longitudine) {
         this.nome = nome;
         this.descrizione = descrizione;
+        this.categoria = categoria;
         this.indirizzo = indirizzo;
-        this.orarioApertura = orarioApertura;
-        this.orarioChiusura = orarioChiusura;
-        this.valutazioneMedia = valutazioneMedia;
+        this.sitoWeb = sitoWeb;
+        this.telefono = telefono;
+        this.email = email;
+        this.orari = orari;
+        this.costo = costo;
+        this.foto = foto;
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
     }
 
-    public String getNome () {
+    public String getNome() {
         return nome;
     }
 
-    public String getDescrizione () {
+    public String getDescrizione() {
         return descrizione;
     }
 
-    public String getIndirizzo () {
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getIndirizzo() {
         return indirizzo;
     }
 
-    public String getOrarioApertura () {
-        return orarioApertura;
+    public String getSitoWeb() {
+        return sitoWeb;
     }
 
-    public String getOrarioChiusura () {
-        return orarioChiusura;
+    public String getTelefono() {
+        return telefono;
     }
 
-    public float getValutazioneMedia () {
-        return valutazioneMedia;
+    public String getEmail() {
+        return email;
     }
 
+    public String getOrari() {
+        return orari;
+    }
+
+    public String getCosto() {
+        return costo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public String getLatitudine() {
+        return latitudine;
+    }
+
+    public String getLongitudine() {
+        return longitudine;
+    }
 }
