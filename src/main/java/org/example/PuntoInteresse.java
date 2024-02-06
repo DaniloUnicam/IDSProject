b1d268c52091a870fb5178ff5872324afb9e0883
@Laiiif <danimatto610@gmail.com>
2024-02-06 18:59:51 +0100

    Modifiche alle classi del progetto

--- src/main/java/org/example/PuntoInteresse.java
+++ src/main/java/org/example/PuntoInteresse.java
@@ -12,12 +12,17 @@ public class PuntoInteresse {
 
 
     private enum tipoStruttura {
-        MUSEO, RISTORANTE, PIZZERIA, ALBERGO, MONUMENTO, STATUA, NEGOZIO, SCUOLA, CHIESA, PARCO, PARCHEGGIO,
-        CENTRO_COMMERCIALE, AUTOVELOX, ATTRAZIONE_TURISTICA, ALTRO
-    };
+        AEROPORTO, ALBERGO, ALTRO, ATTRAZIONE_TURISTICA, AUTOVELOX, CAMPO_BASEBALL,
+        CAMPO_BASKET, CAMPO_BOCCE, CAMPO_BOCCETTE, CAMPO_BOWLING, CAMPO_CALCIO,
+        CAMPO_CRICKET, CAMPO_GOLF, CAMPO_HOCKEY, CAMPO_LACROSSE,
+        CAMPO_PALLAMANO, CAMPO_PALLAVOLO, CAMPO_RUGBY, CAMPO_SOFTBALL, CAMPO_SQUASH,
+        CAMPO_TENNIS, CASCATA, CASERMA_CARABINIERI, CASERMA_POLIZIA,
+        CATENE_MONTUOSE, CENTRO_COMMERCIALE, CHIESA, CINEMA, COMUNE, FARMACIA,
+        FERMATA_BUS, FERMATA_METRO, FERMATA_TRAM, FERMATA_TRENO,
+        MONUMENTO, MUSEO, NEGOZIO, OSPEDALE, PALESTRA, PARCHEGGIO,
+        PARCO, PISCINA, PIZZERIA, RISTORANTE, SCUOLA, STADIO, STATUA
 
-    private float valutazione;
-    private PosizioneSatellitare posizione;
+    };
 
     public PuntoInteresse (String ID, String descrizione, Orario orarioApertura, Orario orarioChiusura, String tipo, float valutazione, PosizioneSatellitare posizione) {
         this.ID = ID;
@@ -53,8 +58,6 @@ public class PuntoInteresse {
     public PosizioneSatellitare getPosizione () {
         return posizione;
     }
-
-
 
     public Contenuto caricaContenuto(Contenuto File, String Commento) {
         return null;
