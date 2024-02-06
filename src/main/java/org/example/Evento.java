9d37420c379bc38c6fa4055fda0868c3b9b9dd16
Abigail31Ramos <abigailestefaniaramos@gmail.com>
2024-02-06 17:20:40 +0100

    Modifica alla classe Evento.java

--- src/main/java/org/example/Evento.java
+++ src/main/java/org/example/Evento.java
@@ -8,4 +8,12 @@ public class Evento {
     private Orario orarioInizio;
     private Orario orarioFine;
 
+    public Evento(String nome, String luogo, String descrizione, PosizioneSatellitare posizione, Orario orarioInizio, Orario orarioFine) {
+        this.nome = nome;
+        this.luogo = luogo;
+        this.descrizione = descrizione;
+        this.posizione = posizione;
+        this.orarioInizio = orarioInizio;
+        this.orarioFine = orarioFine;
     }
+}
